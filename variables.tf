f1353b6005f2b7aadf4c2ec4751e728450a7e1cf
Ricardo <augustineri93@gmail.com>
2022-12-25 00:58:33 -0400

    fix: removed nullable argument from variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "region" {
   type        = string
   description = "AWS default region"
-  nullable    = false
 }
 
 variable "role_arn" {
   type        = string
   description = "AWS account role to assume when creating resources"
-  nullable    = false
 }
