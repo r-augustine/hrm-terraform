48aa596dafcf7641816b405757fa7b8bfe4c2c62
Ricardo <augustineri93@gmail.com>
2022-12-25 01:04:36 -0400

    fix: remove default role data block

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-data "aws_iam_role" "admin_role" {
-  name = "Administrator"
-}
-
 module "hrm" {
   source   = "app.terraform.io/r-augustine/hrm/aws"
   version  = "0.0.3"
   region   = var.region
-  role_arn = data.aws_iam_role.admin_role.arn
+  role_arn = var.role_arn
 }
