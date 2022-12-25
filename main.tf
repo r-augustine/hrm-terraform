3637de4f79c7a8ba8aca70999d9e239598c31423
Ricardo <augustineri93@gmail.com>
2022-12-24 23:53:58 -0400

    feat: added hrm module

---
+++ main.tf
@@ -0,0 +1,10 @@
+data "aws_iam_role" "admin_role" {
+  name = "Administrator"
+}
+
+module "hrm" {
+  source   = "app.terraform.io/r-augustine/hrm/aws"
+  version  = "0.0.1"
+  region   = var.region
+  role_arn = data.aws_iam_role.admin_role.arn
+}
